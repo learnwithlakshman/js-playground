210facc88fc983b4730506a65f5c89e3c49cbad4
M Rajesh <m.rajeshrajz@gmail.com>
2020-05-27 14:38:16 +0530

    added html templates and .js files

---
+++ js/ipl.js
@@ -0,0 +1,4 @@
+let labels = [];
+
+const base_url = 'https://ipl2020-stat.herokuapp.com/ipl2020/team/labels';
+
---
+++ js/main.js
@@ -0,0 +1,6 @@
+let sum = document.querySelector("#sum");
+let sub = document.querySelector("#sub");
+let mul = document.querySelector("#mul");
+let calculateEmi = document.querySelector("#calculateEMI");
+
+
